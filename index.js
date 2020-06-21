67ddfa3d6d96bbadb9a8bae4a56a26e67b6965a8
Cornelis de Mooij <codemo@gmail.com>
2020-06-21 16:56:15 +0200

    Added function example

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 console.log("Hallo Young Colfield!")
 
+// Strings, concatenation & templates:
 const firstName = 'Bob';
 const lastName = 'Jones';
 const age = 45;
@@ -7,4 +8,19 @@ const description1 = 'This is ' + firstName + ' ' + lastName + ' and he is ' + a
 const description2 = `This is ${firstName} ${lastName} and he is ${age} years old.`;
 console.log(description1);
 console.log(description2);
+
+// Functions:
+function mijnNieuweFunctie(eersteInput, tweedeInput) {
+	console.log(eersteInput)
+	const resultaat = eersteInput + tweedeInput;
+	return resultaat;
+}
+const a = "whoa";
+let b = "omg";
+console.log(a)
+console.log(b)
+b = mijnNieuweFunctie(a,b);
+console.log(a)
+console.log(b)
+
 
