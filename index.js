5b14eab3199bc36f6722cc4d252aae9321b1ba57
Cornelis de Mooij <codemo@gmail.com>
2020-06-21 16:22:54 +0200

    Added string template example

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log("Hallo Young Colfield!")
+
+const firstName = 'Bob';
+const lastName = 'Jones';
+const age = 45;
+const description1 = 'This is ' + firstName + ' ' + lastName + ' and he is ' + age + ' years old.';
+const description2 = `This is ${firstName} ${lastName} and he is ${age} years old.`;
+console.log(description1);
+console.log(description2);
+
